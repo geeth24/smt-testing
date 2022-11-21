d5d6425e6fd2b064e543541e639381c0ed3a5f87
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-11-21 00:36:38 -0600

    Change SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <title key="title">Testing | Smart Math Tutoring</title>
+        <link rel="icon" type="image/png" href="/smtlogo.png" />
       </Head>
       <body className="bg-slate-900 body-font font-Inter h-full">
         <Main />
